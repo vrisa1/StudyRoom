b3c1df4b42887ed0cb36b9867bf0e9819836deda
vrisa1 <106835493+vrisa1@users.noreply.github.com>
2023-10-31 12:52:35 -0300

    Nav-Bar
    version1

---
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit{
+  
+  @Input() isUser: boolean = true; 
+
+  ngOnInit(): void {
+    
+  }
+  constructor(private router: Router){}
+
+  //rutas
+  
+
+}
