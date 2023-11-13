3f6508dc7fe822ba85271e193e7992e263df10f7
vrisa1 <106835493+vrisa1@users.noreply.github.com>
2023-11-12 23:55:24 -0300

    Actualizaciones varias
    LandingPage, MainPage, Footer, NavBar y Error404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
