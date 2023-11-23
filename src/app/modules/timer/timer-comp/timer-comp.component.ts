4bb18a4c92ccfd7ce521759c36f54f63d758568f
AgostinaFR <afenoyrivas@gmail.com>
2023-11-23 04:17:12 -0300

    Más cambios del temporizador
    Se agregó la funcionalidad de ciclos de tiempo.
    Se agregó la sección de configuración de ciclos.
    Cambio de audio para alarma de trabajo.
    Detalles de estilo.
    Más comentarios.

--- src/app/modules/timer/timer-comp/timer-comp.component.ts
+++ src/app/modules/timer/timer-comp/timer-comp.component.ts
@@ -12,10 +12,13 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
         transform: 'translateX(-0%)'
       })),
       state('two', style({
-        transform: 'translateX(-33.3%)'
+        transform: 'translateX(-25%)'
       })),
       state('three', style({
-        transform: 'translateX(-66.6%)'
+        transform: 'translateX(-50%)'
+      })),
+      state('four', style({
+        transform: 'translateX(-75%)'
       })),
       transition('* <=> *', [
         animate('500ms ease')
@@ -33,40 +36,46 @@ export class TimerCompComponent {
 
   workDuration = 25
   breakDuration = 5
-  minutes = this.workDuration // - 1 ??
+  minutes = this.workDuration
   seconds = 0
+  totalCycles = 4
+  currentCycle = 1
 
   //MENSAJE DE ESTADO (POMODORO <=> DESCANSO)
 
-  message = 'Pomodoro'
+  stage = 'Pomodoro'
 
   //VARIABLE DE INTERVALO (FUNCIONALIDAD TEMPORIZADOR)
 
-  interval: any
+  interval: any //Almacena en ID del intervalo.
 
   //BOOLEANOS DE ESTADO DEL TEMPORIZADOR (PAUSA, STOP, PLAY)
 
   running = false
-  paused = false;
+  paused = false
 
-  //LIMITES DE TIEMPO EN CONFIGURACIÓN
+  //LÍMITES DE TIEMPO EN CONFIGURACIÓN
 
   minFocusReached = false
   maxFocusReached = false
   minBreakReached = false
   maxBreakReached = false
 
+  //LÍMITES DE CANTIDAD DE CICLOS
+  minCycles = false
+  maxCycles = false //cual es el maximo??
+
   //BOTONES "SIGUIENTE" Y "ATRÁS" (ANIMACIONES) 
 
   toggle(pageNumber: string){
-    this.pageNumber = pageNumber;
+    this.pageNumber = pageNumber
   }
 
   //BOTONES "-" Y "+" (CONFIGURACIÓN DE LÍMITE DE TIEMPO)
 
   increaseFocusTime(){ //Botón "+" en configuración de trabajo.
     this.workDuration += 1
-    this.minutes = this.workDuration //this.workDuration - 1 ?? Actualiza minutos del contador/temporizador.
+    this.minutes = this.workDuration //Actualiza minutos del contador/temporizador.
     if(this.workDuration == 60){ //No permite más de 60 minutos.
       this.maxFocusReached = true
     }
@@ -77,7 +86,7 @@ export class TimerCompComponent {
 
   decreaseFocusTime(){ //Botón "-" en configuración de trabajo.
     this.workDuration -= 1
-    this.minutes = this.workDuration //this.workDuration - 1 ?? Actualiza minutos del contador/temporizador.
+    this.minutes = this.workDuration //Actualiza minutos del contador/temporizador.
     if(this.workDuration == 1){ //No permite menos de 1 minuto.
       this.minFocusReached = true
     }
@@ -106,6 +115,28 @@ export class TimerCompComponent {
     }
   }
 
+  //BOTONES DE CONFIGURACIÓN DE CICLOS
+
+  increaseTotalCycles(){
+    this.totalCycles += 1
+    //   if(this.totalCycles == ){
+    //  this.maxCycles = true
+    //}
+    if(this.totalCycles == 2){
+     this.minCycles = false
+    }
+  }
+
+  decreaseTotalCycles(){
+    this.totalCycles --
+    if(this.totalCycles == 1){
+     this.minCycles = true
+    }
+    // if(this.totalCycles ==){
+    //  this.maxCycles = false
+    // }
+  }
+
   //BOTONES "PAUSA", "STOP"
 
   interrupt(){
@@ -121,15 +152,16 @@ export class TimerCompComponent {
     clearInterval(this.interval)
     this.minutes = this.workDuration
     this.seconds = 0
+    this.stage = "Pomodoro"
   }
 
   //FUNCIONALIDAD
 
-  start(){ //TERMINAR
+  start(){ //TERMINAR  -----CICLOS-----
 
     this.running = true
 
-    this.interval = setInterval(() => counter(), 1000) //intervalo o suscripcion?? Comienza el conteo cada 1 segundo.
+    this.interval = setInterval(() => counter(), 1000) //Comienza el conteo cada 1 segundo.
 
     const counter = () => { //Función ejecutada dentro del intervalo.
 
@@ -138,29 +170,42 @@ export class TimerCompComponent {
         this.seconds -- //se decrementa 1 segundo.
         //console.log(this.interval)
 
-        if(this.seconds == -1 && this.minutes == 0){ //Cuando el tiempo de trabajo se termina,
-          this.message = 'Descanso' //se cambia el estado a descanso,
-
-          this.playBreakAlarm()
-          //aca deberia sonar la alarma
-
+        if(this.seconds == -1 && this.minutes == 0 && this.stage == "Pomodoro"){ //Cuando el tiempo de trabajo se termina,
+          this.playBreakAlarm() //suena la alarma,
+          this.stage = 'Descanso' //se cambia el estado a descanso,
           this.minutes = this.breakDuration //se asigna la duración de descanso a la variable minutos,
           this.seconds = 0 //se inicializa la variable segundos en 0.
         }
-        else if (this.seconds == -1) { //Cuando los segundos se vuelven negativos,
+
+        else if(this.seconds == -1 && this.minutes == 0 && this.stage == "Descanso"){ //Cuando el tiempo de descanso se termina,
+          
+          this.currentCycle ++ //se aumenta el contador de ciclos.
+
+          if(this.currentCycle > this.totalCycles){ //Si ya se cumplió la cantidad de ciclos previamente configurada,
+            this.clear() //se detiene el temporizador,
+            this.currentCycle = 1 //se reinicia el contador de ciclos.
+          }
+          else{ //Si todavía no se cumplió la cantidad de ciclos,
+            this.stage = 'Pomodoro' //se cambia el estado a descanso,
+            this.minutes = this.workDuration //se asigna la duración de descanso a la variable minutos,
+            this.seconds = 0 //se inicializa la variable segundos en 0.
+          }
+
+          this.playWorkAlarm() //Suena la alarma.
+
+        }
+
+        else if(this.seconds == -1){ //Cuando los segundos se vuelven negativos,
           this.seconds = 59 //se vuelve a empezar desde 59,
           this.minutes-- //se decrementa 1 minuto.
         }
-          
-      
-       
 
       }
-      
-
     }
   }
   
+  //FUNCIONES DE ALARMA (AUDIO DEL TEMPORIZADOR)
+
   playWorkAlarm(){ //Dispara la alarma de trabajo.
     let audio = new Audio()
     audio.src = "../assets/workAlarm.mp3"
@@ -176,16 +221,3 @@ export class TimerCompComponent {
   }
 
 }
-
-
-// if(this.seconds == -1){ //cuando los segundos se vuelven negativos
-
-//   this.seconds = 59 //se vuelve a empezar
-//   this.minutes-- //se decrementa un minuto
-
-//   if(this.minutes == -1){ //cuando se terminan de contar los minutos
-
-//     this.message = 'Descanso' //se cambia el estado a descanso
-//   }
-
-// }
